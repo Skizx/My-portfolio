0d2e8d0dcafd23d1a7bcc09c62ddc4606683175c
KevinC <k.civiletti.gj@gmail.com>
2023-05-04 16:40:47 +0200

    Page création non fini

--- src/Pages/Créations.js
+++ src/Pages/Créations.js
@@ -1,6 +1,8 @@
 import '../Components/_creations.scss';
 import React, { useEffect, useState } from 'react';
-import Error from '../Pages/Error'
+import Error from '../Pages/Error';
+import { AiOutlineCaretDown, AiOutlineCaretUp } from 'react-icons/ai'
+import About from '../Components/About/About';
 import creationData from '../Data/créationData.json';
 import { useParams } from 'react-router-dom';
 
@@ -8,6 +10,7 @@ const Créations = () => {
 
     const { id } = useParams()
     const [creation, setCreation] = useState();
+    const [isOpen, setIsOpen] = useState(false);
 
     useEffect(() => {
         const oneCreation = creationData.find((item) => item.id === id)
@@ -25,6 +28,32 @@ const Créations = () => {
                     <img src={creation.picture} alt={creation.title}/>
                 </figure>
             </div>
+                <section id='titledescription' className='description-container'>
+                <About 
+                deviderTitle= ' Description '
+                deviderText= ' Ce que le client demande'
+                />        
+                </section>
+                <section id="description" className="description-block">
+                    <div className="wrapper">
+                    <p>{creation.description}</p>
+                    <div className="txt-center dropdown-container">
+                    <button className='btn-stack' onClick={() => setIsOpen((prev) => !prev)}>
+                    Stacks utilisées {isOpen ? <AiOutlineCaretUp className='dropdown-arrow' /> : <AiOutlineCaretDown className='dropdown-arrow' />}
+                    </button>
+
+                    {isOpen && 
+                        <div className="dropdown-block">
+                            {creation.stacks.map((item, i) => 
+                            <div className='stacks' key={i}>
+                            <h5 className='stacks-title'>{item}</h5>
+                            </div>
+                            )}
+                        </div>
+                    }
+                    </div>
+                    </div>
+                </section>
         </div>
     );
 };
