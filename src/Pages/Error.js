ad248e058430e5782ec8b7d6447525099f7d49ca
KevinC <k.civiletti.gj@gmail.com>
2023-05-07 17:43:59 +0200

    Création page Erreur

--- src/Pages/Error.js
+++ src/Pages/Error.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import '../Components/_error.scss';
 
-const Error = () => {
-    return (
-        <div>
-            ERROR
-        </div>
-    );
-};
+const ErrorPage = () => {
+  return (
+    <>
+      <div className="error-container">
+        <h1>404</h1>
+        <p>Oups! La page que vous demandez n'existe pas.</p>
+        <Link to="/" className="error-container_link">
+          Retourner sur la page d'accueil
+        </Link>
+      </div>
+    </>
+  )
+}
 
-export default Error;
+export default ErrorPage
